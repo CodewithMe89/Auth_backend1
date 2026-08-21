a313d20fc08be553129e527f26dd234cff12f221
CodewithMe89 <keshav9899828464@gmail.com>
2026-08-21 19:20:28 +0530

    normal change

--- src/Routers/AuthRouter.js
+++ src/Routers/AuthRouter.js
@@ -28,7 +28,7 @@ authRouter.post('/signup', async (req, res) => {
         await newUser.save();
         res.status(200).json({message:"User signed up successfully!", newUser})
     } catch (err) {
-        res.status(500).json({ err: "Failed to signup",err:err.message })
+        res.status(500).json({ message: "Failed to signup",err:err.message })
     }
 })
 
